c918c1429ee43b981320c460b3fd79750a691748
Jamescog <jamescog72@gmail.com>
2025-04-14 15:21:06 +0300

    Refactor DNS query functions and add error handling for question bytes generation

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
+	"math/rand"
 	"strings"
+)
+
+const (
+	Class int = 1
+	Type  int = 1
 )
 
 type DNSHeader struct {
@@ -22,7 +29,15 @@ type DNSQuestion struct {
 }
 
 func main() {
+	domain := "www.example.com"
 
+	query, err := buildQuery(domain, Type)
+
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fmt.Printf("Query: %v", query)
 }
 
 func headerToBytes(header DNSHeader) []byte {
@@ -35,6 +50,29 @@ func headerToBytes(header DNSHeader) []byte {
 	binary.Write(&buf, binary.BigEndian, header.numAdditionals)
 
 	return buf.Bytes()
+}
+
+func questionToBytes(question DNSQuestion) ([]byte, error) {
+	var buf bytes.Buffer
+
+	_, err := buf.Write(question.name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to write question name: %w", err)
+	}
+
+	err = binary.Write(&buf, binary.BigEndian, question.fieldType)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to write question type: %w", err)
+	}
+
+	err = binary.Write(&buf, binary.BigEndian, question.fieldClass)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to write question class: %w", err)
+	}
+
+	return buf.Bytes(), nil
 }
 
 func encodeDnsName(name string) []byte {
@@ -53,4 +91,43 @@ func encodeDnsName(name string) []byte {
 
 	buf.WriteByte(0)
 	return buf.Bytes()
+}
+
+func buildQuery(domainName string, recordType int) ([]byte, error) {
+
+	var buf bytes.Buffer
+
+	nameByte := encodeDnsName(domainName)
+	id := rand.Intn(65535)
+	recurrsionDesired := 1 << 8
+	header := DNSHeader{}
+	header.id = uint16(id)
+	header.flags = uint16(recurrsionDesired)
+	header.numQuestions = uint16(1)
+	question := DNSQuestion{}
+	question.name = nameByte
+	question.fieldType = uint16(recordType)
+	question.fieldClass = uint16(Class)
+
+	headerByte := headerToBytes(header)
+	questionByte, err := questionToBytes(question)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to generating question bytes: %w", err)
+	}
+
+	_, err = buf.Write(headerByte)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to write header bytes: %w", err)
+	}
+
+	_, err = buf.Write(questionByte)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to write question bytes: %w", err)
+	}
+
+	return buf.Bytes(), nil
+
 }
