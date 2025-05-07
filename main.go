1d8ac8dab3d5a7b82fccfe57083db423c70c0a82
Jamescog <jamescog72@gmail.com>
2025-05-07 16:19:52 +0300

    wait

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"fmt"
 	"math/rand"
+	"net"
 	"strings"
 )
 
@@ -28,27 +29,43 @@ type DNSQuestion struct {
 	fieldClass uint16
 }
 
+type DNSRecord struct {
+	name       []byte
+	fieldType  uint16
+	fieldClass uint16
+	feildTTL   uint32
+	rdLength   uint16
+	rData      []byte
+}
+
 func main() {
 	domain := "www.example.com"
 
 	query, err := buildQuery(domain, Type)
 
 	if err != nil {
-		fmt.Printf("Error: %v", err)
+		fmt.Print("Error occured")
+	}
+	res, err := sendQuery(query, "8.8.8.8:53")
+	header, err := parseHeader(res)
+
+	if err != nil {
+		fmt.Printf("Error occured: %v", err)
+		return
 	}
 
-	fmt.Printf("Query: %v", query)
+	fmt.Printf("Header: %v", header)
+
 }
 
 func headerToBytes(header DNSHeader) []byte {
 	var buf bytes.Buffer
-
 	binary.Write(&buf, binary.BigEndian, header.id)
 	binary.Write(&buf, binary.BigEndian, header.flags)
 	binary.Write(&buf, binary.BigEndian, header.numQuestions)
+	binary.Write(&buf, binary.BigEndian, header.numAnswers)
 	binary.Write(&buf, binary.BigEndian, header.numAuthorities)
 	binary.Write(&buf, binary.BigEndian, header.numAdditionals)
-
 	return buf.Bytes()
 }
 
@@ -100,6 +117,8 @@ func genHeader() DNSHeader {
 	header.id = uint16(id)
 	header.flags = uint16(recurrsionDesired)
 	header.numQuestions = uint16(1)
+	header.numAnswers = 0
+	header.numAdditionals = 0
 	return header
 }
 func genQuestion(name string, recordType int) DNSQuestion {
@@ -137,4 +156,84 @@ func buildQuery(domainName string, recordType int) ([]byte, error) {
 
 	return buf.Bytes(), nil
 
+}
+
+func parseHeader(data []byte) (DNSHeader, error) {
+	if len(data) < 12 {
+		return DNSHeader{}, fmt.Errorf("response is too short: %d", len(data))
+	}
+
+	reader := bytes.NewReader(data)
+	var header DNSHeader
+
+	err := binary.Read(reader, binary.BigEndian, &header.id)
+
+	if err != nil {
+		return DNSHeader{}, fmt.Errorf("Error reading header: %v", err)
+	}
+
+	err = binary.Read(reader, binary.BigEndian, &header.flags)
+
+	if err != nil {
+		return DNSHeader{}, fmt.Errorf("Error reading flags: %v", err)
+	}
+
+	err = binary.Read(reader, binary.BigEndian, &header.numQuestions)
+
+	if err != nil {
+		return DNSHeader{}, fmt.Errorf("Error reading questions: %v", err)
+	}
+
+	err = binary.Read(reader, binary.BigEndian, &header.numAnswers)
+
+	if err != nil {
+		return DNSHeader{}, fmt.Errorf("Error reading answers: %v", err)
+	}
+
+	err = binary.Read(reader, binary.BigEndian, &header.numAuthorities)
+
+	if err != nil {
+		return DNSHeader{}, fmt.Errorf("Error reading authorities: %v", err)
+	}
+
+	err = binary.Read(reader, binary.BigEndian, &header.numAdditionals)
+
+	if err != nil {
+		return DNSHeader{}, fmt.Errorf("Error reading addtionals: %v", err)
+	}
+
+	return header, nil
+
+}
+
+func sendQuery(query []byte, server string) ([]byte, error) {
+
+	addr, err := net.ResolveUDPAddr("udp", server)
+
+	if err != nil {
+		return nil, fmt.Errorf("failded to resolve server address: %w", err)
+	}
+
+	conn, err := net.DialUDP("udp", nil, addr)
+
+	if err != nil {
+		return nil, fmt.Errorf("failded to dial UPD: %w", err)
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Write(query)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to send query: %w", err)
+	}
+
+	response := make([]byte, 512)
+	n, _, err := conn.ReadFromUDP(response)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response: %w", err)
+	}
+
+	return response[:n], nil
 }
